e4477daa79ea93dee94dc5c3bc7f1b966129e5de
MartinCrane <martin.crane@gmail.com>
2017-02-16 15:16:18 -0500

    Done.

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -1,2 +1,10 @@
 class CreateArtists < ActiveRecord::Migration
+  has_many :songs
+  has_many :genres, through: :songs
+  
+  def change
+    create_table :artists do |t|
+      t.string :name
+    end
+  end
 end
